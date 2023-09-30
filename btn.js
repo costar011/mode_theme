17fe10a8fbb1a24f015b66425a591ba0941eaa1c
costar011 <dnaer456@gmail.com>
2023-09-30 20:20:09 +0900

    reset

--- btn.js
+++
@@ -1,9 +0,0 @@
-const btn = document.querySelector('#btn');
-
-btn.addEventListener("click", (e) => {
-    if(btn.checked == true) {
-        document.body.setAttribute("data-theme", "dark");
-    } else {
-        document.body.setAttribute("data-theme", " ");
-    }
-});
