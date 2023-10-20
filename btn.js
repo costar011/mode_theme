271aed33bac74a46dd71d894bbfbb692c5600b50
costar011 <dnaer456@gmail.com>
2023-10-21 01:15:59 +0900

    js

---
+++ btn.js
@@ -0,0 +1,7 @@
+const btn = document.querySelector('#btn');
+
+btn.addEventListener("click", (e) =>  {
+    if(btn.checked == true) {
+        document.body.setAttribute("data-theme", "dark");
+    }
+})
